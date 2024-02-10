025c7343920ab0f05edb2cc67102496e6b2d47aa
LewisSMcCallum <lewismccallum@gmail.com>
2024-02-10 15:50:38 +0000

    10x helium heat

--- src/TaskBasedRadiationHydrodynamicsSimulation.cpp
+++ src/TaskBasedRadiationHydrodynamicsSimulation.cpp
@@ -786,7 +786,7 @@ const double h0 = ionization_variables.get_ionic_fraction(ION_H_n);
     const double T4 = 1.e-4*temp;
     const double he0 = ionization_variables.get_ionic_fraction(ION_He_n);
     const double hep = ionization_variables.get_ionic_fraction(ION_He_p1);
-     gain += AHe*ionization_variables.get_heating(HEATINGTERM_He) *
+     gain += 10*AHe*ionization_variables.get_heating(HEATINGTERM_He) *
                         ionization_variables.get_number_density() /
                         inverse_volume *
                         ionization_variables.get_ionic_fraction(ION_He_n);
@@ -832,25 +832,25 @@ const double nenhp = ne*n*(1-h0);
   loss = std::max(loss,0.0);
 
 
-    if (ionization_variables.get_heating(HEATINGTERM_He) > 0.0 && n > 0.0) {
-    std::cout << "PHOTO = " << AHe*ionization_variables.get_heating(HEATINGTERM_He) *
-                        ionization_variables.get_number_density() /
-                        inverse_volume *
-                        ionization_variables.get_ionic_fraction(ION_He_n) << std::endl;
-    std::cout << "Lyman Alpha = " << pHots * 1.21765423e-18 * alpha_e_2sP * nenhep/inverse_volume << std::endl;
-    std::cout << "Hydrogen photo = " << ionization_variables.get_heating(HEATINGTERM_H) *
-                        ionization_variables.get_number_density() /
-                        inverse_volume *
-                        ionization_variables.get_ionic_fraction(ION_H_n) << std::endl;
-    std::cout << "Lhep  = " << Lhep/inverse_volume << std::endl;
+  //   if (ionization_variables.get_heating(HEATINGTERM_He) > 0.0 && n > 0.0) {
+  //   std::cout << "PHOTO = " << AHe*ionization_variables.get_heating(HEATINGTERM_He) *
+  //                       ionization_variables.get_number_density() /
+  //                       inverse_volume *
+  //                       ionization_variables.get_ionic_fraction(ION_He_n) << std::endl;
+  //   std::cout << "Lyman Alpha = " << pHots * 1.21765423e-18 * alpha_e_2sP * nenhep/inverse_volume << std::endl;
+  //   std::cout << "Hydrogen photo = " << ionization_variables.get_heating(HEATINGTERM_H) *
+  //                       ionization_variables.get_number_density() /
+  //                       inverse_volume *
+  //                       ionization_variables.get_ionic_fraction(ION_H_n) << std::endl;
+  //   std::cout << "Lhep  = " << Lhep/inverse_volume << std::endl;
 
-      if (gain != gain) {
-        cmac_error("GAIN IS NAN");
-      } 
-      if (loss != loss) {
-        cmac_error("Loss in NAN");
-      }
-  }
+  //     if (gain != gain) {
+  //       cmac_error("GAIN IS NAN");
+  //     } 
+  //     if (loss != loss) {
+  //       cmac_error("Loss in NAN");
+  //     }
+  // }
   } else {
     loss = radiative_cooling->get_cooling_rate(temp)*n*n/inverse_volume;
   }
