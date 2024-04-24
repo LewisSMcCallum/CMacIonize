95e90ea6f6d57e1523733162544b94bdf28d0b1b
LewisSMcCallum <lewismccallum@gmail.com>
2024-04-24 16:00:42 +0100

    check all t > 0

--- src/CMacIonizeSnapshotDensityFunction.cpp
+++ src/CMacIonizeSnapshotDensityFunction.cpp
@@ -405,6 +405,7 @@ void CMacIonizeSnapshotDensityFunction::initialize() {
                        ", %" PRIuFAST32 ")!",
                        ix, iy, iz);
           }
+          cmac_assert(_cartesian_grid[ix][iy][iz].get_temperature() > 0.0);
         }
       }
     }
