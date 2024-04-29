1909cc56c3cfa2ab4070f4c4523200b18fcab28c
LewisSMcCallum <lewismccallum@gmail.com>
2024-04-29 11:19:28 +0100

    error if rho =0

--- src/Hydro.hpp
+++ src/Hydro.hpp
@@ -41,7 +41,7 @@
 #define SAFE_HYDRO_VARIABLES
 
 /*! @brief Uncomment this to activate the flux limiter. */
-//#define FLUX_LIMITER 2.
+#define FLUX_LIMITER 2.
 
 /**
  * @brief Hydro related functionality.
@@ -322,7 +322,9 @@ public:
 #endif
       }
     }
-
+    if (density == 0) {
+      cmac_error("Setting rho = 0. Stopping.")
+    }
     hydro_state.set_primitives_density(density);
     hydro_state.set_primitives_velocity(velocity);
     cmac_assert_message(pressure > 0.0,"ABOUT TO SET P=0, energy=%g,kin=%g",hydro_state.get_conserved_total_energy(),0.5 * CoordinateVector<>::dot_product(
